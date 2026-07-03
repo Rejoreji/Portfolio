1c20fde5c921d409ad55958a082c4dddc0d0a3cc
Rejo Reji <rejosmac@Rejos-MacBook-Air.local>
2026-07-03 16:18:41 +0530

    updated the profile picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import profile from "../assets/profilePic.jpg"
+import profile from "../assets/profilePic2.jpg"
 import { FiGlobe } from 'react-icons/fi'
 
 const Hero = () => {
@@ -14,11 +14,14 @@ const Hero = () => {
             </p>
         </div>
         <div className="flex flex-col items-center gap-10">
-            <img src={profile} alt="" srcset="" className="w-100 h-120 object-cover rounded-[50px]" />
+            <div className="w-100 h-120 overflow-hidden rounded-[50px]">
+                <img src={profile} alt="" srcset="" className="w-200 h-150 object-cover " />
+            </div>
+            
             <div className="flex items-center gap-5 bg-gray-200 px-5 py-2 rounded-full w-full justify-center">
                 <FiGlobe size={70} className="text-gray-800" />
-                <span className="text-gray-800 font-light text-2xl">
-                    Currently based in India
+                <span className="text-gray-800 font-light text-[18px]">
+                    Currently based in Bangalore, India
                 </span>
             </div>
         </div>
